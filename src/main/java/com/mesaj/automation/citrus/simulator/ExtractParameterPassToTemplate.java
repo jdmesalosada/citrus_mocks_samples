cc73f805ceecc9b1b38279f9e689bc09a328492f
Julian Mesa <julianmesa@Julians-MacBook-Air.local>
2019-01-27 16:37:10 -0500

    removed commented code

--- src/main/java/com/mesaj/automation/citrus/simulator/ExtractParameterPassToTemplate.java
+++ src/main/java/com/mesaj/automation/citrus/simulator/ExtractParameterPassToTemplate.java
@@ -11,26 +11,17 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @Scenario("ExtractParameter2")
 @RequestMapping(value = "/services/rest/v1/extractParameter2", method = RequestMethod.POST)
 public class ExtractParameterPassToTemplate extends AbstractSimulatorScenario {
+
     @Override
     public void run(ScenarioRunner scenario) {
-
-/*        scenario
-                .http()
-                .receive(con -> con.post().extractFromPayload("$.id", "id"));
-
-        scenario.http().send(builder -> builder.response()
-                .statusCode(200).payload("el ide del usuario es ${id}"));*/
-
         scenario
                 .http()
                 .receive(con -> con.post().contentType("application/json;charset=UTF-8")
                         .extractFromPayload("$.id", "id")
                 );
 
-
         String message = scenario.getTestContext().getVariable("id");
         System.out.println("variable capturada printed:  " + message);
-        //"${id}"
 
         scenario.http().send(builder -> builder.response()
                 .statusCode(200).payload(new ClassPathResource("templates/customerAuthenticationGet.xml")));
